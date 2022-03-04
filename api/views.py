c2f48d0222759d54bc896832a79975fd0e8e401b
Bibek Paudyal <bibekpaudyal23@gmail.com>
2022-03-04 10:16:47 +0545

    completed api for all the reports

--- api/views.py
+++ api/views.py
@@ -33,8 +33,6 @@ class CountrySalesView(viewsets.ModelViewSet):
 class AverageSalesAPIView(viewsets.ModelViewSet):
     serializer_class = PetroleumDetailSerializer
     def list(self,request):
-        #annotating the sale__sum based on the value country of our model
-        #and ordering in descending/ascending order of total sale sum and slicing the first 3 records only
         avg1 = PetroleumDetails.objects.filter(year__range = (2007,2010)).exclude(sale = 0).values('petroleum_product').annotate(avg_sale = Avg('sale')).annotate(year_range = Value('2007-2010',output_field=CharField()))
         avg2 = PetroleumDetails.objects.filter(year__range = (2011,2014)).exclude(sale = 0).values('petroleum_product').annotate(avg_sale = Avg('sale')).annotate(year_range = Value('2011-2014',output_field=CharField()))
         final_average = [x for x in itertools.chain.from_iterable(itertools.zip_longest(avg1,avg2)) if x]
