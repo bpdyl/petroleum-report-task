7878aac6ec0ddb0edde44b31c5ddf72e6756b98b
Bibek Paudyal <bibekpaudyal23@gmail.com>
2022-03-05 06:35:38 +0545

    some report generating task

--- core/views.py
+++ core/views.py
@@ -12,72 +12,76 @@ class IndexView(ListView):
     template_name = 'index.html'
     def get_context_data(self,**kwargs):
         context = super().get_context_data(**kwargs)
-        r = requests.get('https://raw.githubusercontent.com/younginnovations/internship-challenges/master/programming/petroleum-report/data.json')
-        records = json.loads(r.content.decode('utf-8'))
-        print("records",records)
-        #We define two lists:
-        # one list for holding the values that we want to insert
-        # records list may contains the values that would have been updated as well so we have distinguished the records to insert and update separately
-        # another list for the new values if data.json had updated values or contains any new values
-        records_to_create = []
-        records_to_update = []
-        
-        # This is where we check if the records are pre-existing,
-        # and add primary keys to the objects if they do
+        try:
+            r = requests.get('https://raw.githubusercontent.com/younginnovations/internship-challenges/master/programming/petroleum-report/data.json')
+            records = json.loads(r.content.decode('utf-8'))
+            print("records",records)
+            #We define two lists:
+            # one list for holding the values that we want to insert
+            # records list may contains the values that would have been updated as well so we have distinguished the records to insert and update separately
+            # another list for the new values if data.json had updated values or contains any new values
+            records_to_create = []
+            records_to_update = []
+            
+            # This is where we check if the records are pre-existing,
+            # and add primary keys to the objects if they do
 
-        records = [
-            {
-                "id": PetroleumDetails.objects.filter(
-                    year = record['year'],
-                    petroleum_product = record['petroleum_product'],
-                    sale = record['sale'],
-                    country = record['country'],
-                ).first().id
+            records = [
+                {
+                    "id": PetroleumDetails.objects.filter(
+                        year = record['year'],
+                        petroleum_product = record['petroleum_product'],
+                        sale = record['sale'],
+                        country = record['country'],
+                    ).first().id
 
-                if PetroleumDetails.objects.filter(
-                    year = record['year'],
-                    petroleum_product = record['petroleum_product'],
-                    sale = record['sale'],
-                    country = record['country'],
-                    ).first() is not None
-                else None,
-                **record,
-            }
-            for record in records
-        ]
-        
-        # This is where we separate our records to our split lists: 
-        # - if the record already exists in the sqlite database (the 'id' primary key), add it to the update list.
-        # - else add it to the create list.
-        [
-            records_to_update.append(record)
-            if record["id"] is not None
-            else records_to_create.append(record)
-            for record in records
-        ]
-        
-        # Remove the 'id' field, as these will all hold a value of None for records to create,
-		# since these records do not already exist in the DB
-        [record.pop("id") for record in records_to_create]
-        
-        #using bulk_create and bulk_update so as all the operation happens in single statement
-        created_records = PetroleumDetails.objects.bulk_create(
-            [PetroleumDetails(**values) for values in records_to_create], batch_size=500
-        )
-        PetroleumDetails.objects.bulk_update(
+                    if PetroleumDetails.objects.filter(
+                        year = record['year'],
+                        petroleum_product = record['petroleum_product'],
+                        sale = record['sale'],
+                        country = record['country'],
+                        ).first() is not None
+                    else None,
+                    **record,
+                }
+                for record in records
+            ]
+            
+            # This is where we separate our records to our split lists: 
+            # - if the record already exists in the sqlite database (the 'id' primary key), add it to the update list.
+            # - else add it to the create list.
             [
-                PetroleumDetails(id=values.get("id"), year = values['year'],
-                petroleum_product = values['petroleum_product'],
-                sale = values['sale'],
-                country = values['country'])
-                for values in records_to_update
-            ],
-            ["year","petroleum_product","sale","country"],
-            batch_size=500
-        )
-        petroleum_details = PetroleumDetails.objects.all()
-        context['all_petroleum'] = petroleum_details
-        return context
+                records_to_update.append(record)
+                if record["id"] is not None
+                else records_to_create.append(record)
+                for record in records
+            ]
+            
+            # Remove the 'id' field, as these will all hold a value of None for records to create,
+            # since these records do not already exist in the DB
+            [record.pop("id") for record in records_to_create]
+            
+            #using bulk_create and bulk_update so as all the operation happens in single statement
+            created_records = PetroleumDetails.objects.bulk_create(
+                [PetroleumDetails(**values) for values in records_to_create], batch_size=500
+            )
+            PetroleumDetails.objects.bulk_update(
+                [
+                    PetroleumDetails(id=values.get("id"), year = values['year'],
+                    petroleum_product = values['petroleum_product'],
+                    sale = values['sale'],
+                    country = values['country'])
+                    for values in records_to_update
+                ],
+                ["year","petroleum_product","sale","country"],
+                batch_size=500
+            )
+            petroleum_details = PetroleumDetails.objects.all()
+            context['all_petroleum'] = petroleum_details
+            return context
+        except:
+            context['error'] = "Something went wrong. Please check your internet connection"
+            return context
 
 
 class TotalSalesView(ListView):
@@ -88,7 +92,8 @@ class TotalSalesView(ListView):
         total_sales = []
         #annotating the sale__sum along with the petroleum product values
         sales = PetroleumDetails.objects.values('petroleum_product').annotate(Sum('sale'))
-        print(sales)
+        # 'SELECT petroleum_product, SUM(sale) AS total_sale FROM reports GROUP BY petroleum_product'
+        print(sales.query)
         context['sales'] = sales
         return context 
 
@@ -116,6 +121,7 @@ class AverageSalesView(ListView):
         avg_data2 = PetroleumDetails.objects.values('petroleum_product').exclude(sale = 0).filter(year__range=(2011,2014)).annotate(avg_sale = Avg('sale')).annotate(year_range = Value('2011-2014',output_field=CharField()))
         #creating the final list that contains the alternating element from avg_data1 and avg_data2
         final_data = [x for x in itertools.chain.from_iterable(itertools.zip_longest(avg_data1,avg_data2)) if x]
+        print(avg_data1.query)
         context['final_data'] = final_data
         return context
 
